103568215eb97a5e863fae717f014390b5117a94
olyesung <olyesung@gmail.com>
2021-12-10 02:33:31 +0900

    server

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
+import http from "http";
 import { ApolloServer } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import express from "express";
 import logger from "morgan";
 import { graphqlUploadExpress } from "graphql-upload";
@@ -9,24 +11,50 @@ import { getUser } from "./users/users.utils";
 const PORT = process.env.PORT;
 
 async function startServer() {
+  const app = express();
+  const httpServer = http.createServer(app);
+
   const apollo = new ApolloServer({
     typeDefs,
     resolvers,
     playground: true,
     introspection: true,
-    context: async ({ req }) => {
-      return {
-        loggedInUser: await getUser(req.headers.token),
-      };
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    context: async (ctx) => {
+      if (ctx.req) {
+        return {
+          loggedInUser: await getUser(ctx.req.headers.token),
+        };
+      } else {
+        const {
+          connection: { context },
+        } = ctx;
+        return {
+          loggedInUser: context.loggedInUser,
+        };
+      }
+    },
+    subscriptions: {
+      onConnect: async ({ token }) => {
+        console.log(token);
+        if (!token) {
+          throw new Error("You can't listen.");
+        }
+        const loggedInUser = await getUser(token);
+        return {
+          loggedInUser,
+        };
+      },
     },
   });
+
   await apollo.start();
-  const app = express();
+  apollo.applyMiddleware({ app });
   app.use(graphqlUploadExpress());
   app.use(logger("tiny"));
-  apollo.applyMiddleware({ app });
   app.use("/static", express.static("uploads"));
-  app.listen({ port: PORT }, () => {
+
+  httpServer.listen({ port: PORT }, () => {
     console.log(
       `🚀Server is running on http://localhost:${PORT} ✅
 👾Graphql: http://localhost:${PORT}${apollo.graphqlPath}`
